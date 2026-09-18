2690fb15dc2134e9322f8502284c2f3c717ddd7a
TomasThrawat <tomastharwat873@gmail.com>
2026-09-18 15:37:27 +0300

    Fix JVM target compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.tomasthrawat.bluetoothfileshare"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.tomasthrawat.bluetoothfileshare"
         minSdk = 26
@@ -12,8 +14,21 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-    buildTypes { release { isMinifyEnabled = false } }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
